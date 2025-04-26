c38898c0fb2bf5646a95f4933c202032fdf1ea4e
yygqzzk <1374174364@qq.com>
2025-04-26 22:15:53 +0800

    feat: project-init

--- group-buy-market-app/src/test/java/xyz/yygqzzk/test/ApiTest.java
+++ group-buy-market-app/src/test/java/xyz/yygqzzk/test/ApiTest.java
@@ -1,19 +1,31 @@
 package xyz.yygqzzk.test;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import xyz.yygqzzk.infrastructure.dao.IGroupBuyActivityDao;
+import xyz.yygqzzk.infrastructure.dao.IGroupBuyDiscountDao;
+import xyz.yygqzzk.infrastructure.dao.po.GroupBuyActivity;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
 
+    @Resource
+    private IGroupBuyActivityDao groupBuyActivityDao;
+
+
     @Test
     public void test() {
-        log.info("测试完成");
+        List<GroupBuyActivity> groupBuyActivities = groupBuyActivityDao.queryGroupBuyActivityList();
+        log.info(JSON.toJSONString(groupBuyActivities));
     }
 
 }
---
+++ group-buy-market-infrastructure/src/main/java/xyz/yygqzzk/infrastructure/dao/IGroupBuyDiscountDao.java
@@ -0,0 +1,16 @@
+package xyz.yygqzzk.infrastructure.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import xyz.yygqzzk.infrastructure.dao.po.GroupBuyDiscount;
+
+import java.util.List;
+
+/**
+ * @author zzk
+ * @version 1.0
+ * @since 2025/4/26
+ */
+@Mapper
+public interface IGroupBuyDiscountDao {
+    List<GroupBuyDiscount> queryGroupBuyDiscountList();
+}
